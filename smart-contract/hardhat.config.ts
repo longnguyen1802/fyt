cea40d45a5158d4d2f7a16ba83ba74eb97ed4e80
longnguyen1802 <longnguyenhsgs@gmail.com>
2024-04-20 15:27:02 +0800

    Update gas report

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  gasReporter: {
+    enabled: true,
+    currency: "ETH",
+    outputFile: "gas-report.txt",
+    noColors: false,
+    // You can customize the gas reporter settings here
+    // Check the Hardhat Gas Reporter documentation for more options
+  },
 };
 
 export default config;
